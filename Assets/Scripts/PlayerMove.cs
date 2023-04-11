0d460cfe49985803abafee04a776c16288d7a9e4
Ricard-Ruiz-TCM <83354017+Ricard-Ruiz-TCM@users.noreply.github.com>
2023-04-11 11:04:10 +0200

    cositas :3

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,11 +6,20 @@ public class PlayerMove : AnimatedState {
         StateTransition toIddle = new StateTransition(
             () => { return false; },
             () => { Debug.Log("TO IDDLE FROM MOVE TRIGGER"); }
-        ); AddTransition(toIddle, PlayerStates.Iddle);
+        );
+        AddTransition(toIddle, PlayerStates.Iddle);
     }
 
-    public override void OnEnter() { Debug.Log("Move OnEnter"); }
-    public override void OnState() { Debug.Log("Move OnUpdate"); }
-    public override void OnExit() { Debug.Log("Move OnExit"); }
+    public override void OnEnter() {
+        Debug.Log("Move OnEnter");
+    }
+
+    public override void OnState() {
+        Debug.Log("Move OnUpdate");
+    }
+
+    public override void OnExit() {
+        Debug.Log("Move OnExit");
+    }
 
 }

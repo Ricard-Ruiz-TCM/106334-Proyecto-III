08fe8d208d4104f739358a606550887678a68bba
Ricard-Ruiz-TCM <rruizc@edu.tecnocampus.cat>
2023-04-19 16:43:24 +0200

    Inicio de los turnos ;3

--- Assets/Scripts/Stage/TurnManager.cs
+++ Assets/Scripts/Stage/TurnManager.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurnManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class TurnManager : MonoBehaviour {
+
+    private int _turn = 0;
+    private List<ITurnElement> _turnList;
+
+    private ITurnElement Element() { return _turnList[_turn]; }
+
+    public void Add(ITurnElement element) {
+        if (!Contains(element))
+            _turnList.Add(element);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Remove(ITurnElement element) {
+        if (Contains(element))
+            _turnList.Remove(element);
     }
+
+    // Unity Update
+    private void Update() {
+        // Turno completado
+        if (Element().TurnCompleted) {
+            NextTurn();
+        }
+    }
+
+    public void NextTurn() {
+        _turn++;
+        if (_turn > _turnList.Count) _turn = 0;
+    }
+
+    private bool Contains(ITurnElement element) {
+        return _turnList.Contains(element);
+    }
+
 }
---
+++ Assets/Scripts/Stage/Turns/ITurnElement.cs
@@ -0,0 +1,8 @@
+﻿
+public interface ITurnElement {
+
+    public bool MoveAction { get; }
+    public bool StandartAction { get; }
+    public bool TurnCompleted { get; }
+
+}

27957da4e21c4e2d0b53fa69574953393c634df4
RocFauconTuron <rfauconi@edu.tecnocampus.cat>
2023-04-20 00:41:54 +0200

    cositas

--- Assets/Scripts/Stage/Grid/InstanciateGrid.cs
+++ Assets/Scripts/Stage/Grid/InstanciateGrid.cs
@@ -11,13 +11,14 @@ public class InstanciateGrid : MonoBehaviour
     public GameObject gridPrefab;
     public float gridPrefabLenght = 10.5f;
     public Vector3 instanceGridPos = new Vector3(0, 0, 0);
-    [SerializeField] int[,] gridArray;
-    public int startX;
-    public int startY;
-    public int endY;
-    public int endX;
-    [SerializeField] LayerMask rayLayerMask;
+
+
+    [SerializeField] Node[,] grid;
     Inspector2dArray array;
+    bool walkable;
+
+    public Material walkableMat;
+    public Material unwalkableMat;
 
     // Start is called before the first frame update
     void Start()
@@ -33,14 +34,25 @@ public class InstanciateGrid : MonoBehaviour
     }
     private void Instanciate()
     {
+        grid = new Node[rows, columns];
         for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < columns; j++)
             {
-                if(!array.columns[j].rows[i])
+                GameObject obj = GameObject.Instantiate(gridPrefab, new Vector3(instanceGridPos.x + scale * i * gridPrefabLenght, 1, instanceGridPos.z + scale * j * gridPrefabLenght), Quaternion.identity);
+                if (!array.columns[j].rows[i])
                 {
-                    GameObject.Instantiate(gridPrefab, new Vector3(instanceGridPos.x + scale * i * gridPrefabLenght, 1, instanceGridPos.z + scale * j * gridPrefabLenght), Quaternion.identity);
+                    walkable = false;
+                    obj.GetComponent<Renderer>().material = walkableMat;
                 }
+                else
+                {
+                    obj.GetComponent<Renderer>().material = unwalkableMat;
+                    walkable = true;
+                }
+                grid[i, j] = new Node(walkable, obj.transform.position);    
+
+
                 //if (Physics.Raycast(new Vector3(instanceGridPos.x + scale * i * gridPrefabLenght, 1, instanceGridPos.z + scale * j * gridPrefabLenght),new Vector3(0,1,0), 10, rayLayerMask))
                 //{
                 //    Debug.Log("obstacle");

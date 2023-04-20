32f8a7388b06cc689a163c60b6ddeb40e503c4a7
RocFauconTuron <rfauconi@edu.tecnocampus.cat>
2023-04-20 13:30:44 +0200

    pulir cosas de la posicion del player en grid+ un poco de pathfinding

--- Assets/Scripts/Stage/Grid/Node.cs
+++ Assets/Scripts/Stage/Grid/Node.cs
@@ -6,10 +6,29 @@ public class Node
 {
     public bool walkable;
     public Vector3 worldPos;
+    public GameObject gridObj;
 
-    public Node(bool _walkable, Vector3 _worldPos)
+    public int gCost;
+    public int hCost;
+
+    public int gridX;
+    public int gridY;
+
+    public Node parent;
+
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
+    }
+
+    public Node(bool _walkable, Vector3 _worldPos,GameObject obj)
     {
         walkable = _walkable;
         worldPos = _worldPos;
+        gridObj = obj;
     }
+
 }

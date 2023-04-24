604595b77c5bf75f3856a477e53ae131f086fdfa
Ricard-Ruiz-TCM <83354017+Ricard-Ruiz-TCM@users.noreply.github.com>
2023-04-24 22:21:55 +0200

    cositas del combate

---
+++ Assets/Scripts/Combat/Skills/Attack.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Attack", menuName = "Combat/Attack")]
+public class Attack : Skill {
+
+    public override void Special() {
+        Debug.Log("Attack special attack");
+    }
+
+}
---
+++ Assets/Scripts/Combat/Skills/Defense.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "new Defense", menuName = "Combat/Defense")]
+public class Defense : Skill {
+
+    public override void Special() {
+        Debug.Log("Defense special attack");
+    }
+
+}

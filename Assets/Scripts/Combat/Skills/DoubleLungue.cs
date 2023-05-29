4e6191ea7dbfffab4773a43bb6ab0e9273985f85
Ricard-Ruiz-TCM <83354017+Ricard-Ruiz-TCM@users.noreply.github.com>
2023-05-29 11:29:56 +0200

    DoubleLungue funcional :3

--- Assets/Scripts/Combat/Skills/DoubleLungue.cs
+++ Assets/Scripts/Combat/Skills/DoubleLungue.cs
@@ -1,11 +1,26 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 [CreateAssetMenu(fileName = "Double Lungue", menuName = "Combat/Skills/Double Lungue")]
 public class DoubleLungue : Skill {
 
+    [SerializeField]
+    private float _damageDelay = 0.25f;
+
     public override void action(BasicActor from, Node to) {
-        // TODO
+        BasicActor target = Stage.StageManager.getActor(to);
+        if (target != null) {
+            from.StartCoroutine(C_DamageAgain(from, target));
+        }
         from.endAction();
+    }
+
+    /** Coroutine para aplicar el segúndo hit un tiempo después */
+    public IEnumerator C_DamageAgain(BasicActor from, BasicActor target) {
+        target.takeDamage((Actor)from, from.totalDamage());
+        yield return new WaitForSeconds(_damageDelay);
+        if (from != null)
+            target.takeDamage((Actor)from, from.totalDamage());
     }
 
 }

c36727bc60cfc95982801644ed657806367ddacb
Antoni Jordi Noguera Recasens <anoguera@edu.tecnocampus.cat>
2023-05-29 19:57:02 +0200

    Stage 0 y logica de las columnas

--- Assets/Scripts/Actor/PilarStatic.cs
+++ Assets/Scripts/Actor/PilarStatic.cs
@@ -1,18 +1,46 @@
-﻿
-
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class PilarStatic : StaticActor {
 
-    BasicActor damageFrom;
+    [SerializeField] private int pilarHeight = 0;
+    private BasicActor damageFrom;
+    private Node pilarNode;
+    private Animator pilarAnimator;
+
+    protected override void Start()
+    {        
+        base.Start();
+        pilarNode = Stage.StageBuilder.getGridNode(this.transform.position);
+        pilarAnimator = this.GetComponent<Animator>();
+    }
 
     public override void onActorDeath() {
+        
+        Vector2 direction = Stage.StageBuilder.getDirection(this.transform.position, damageFrom.transform.position);
+        direction = direction * pilarHeight;
+        Node endPoint = Stage.StageBuilder.getGridNode(pilarNode.x + (int)direction.x, pilarNode.y + (int)direction.y);
 
-        base.onActorDeath();
+        this.transform.LookAt(Stage.StageBuilder.getGridPlane(endPoint).transform.position);
+
+        pilarAnimator.SetTrigger("GoDown");
+
+        List<Node> nodesToSetNotWalkable = Stage.Pathfinder.FindPath(pilarNode, endPoint, false);
+
+        Stage.Grid.changeNodeType(endPoint.x, endPoint.y, Array2DEditor.nodeType.X);
+
+        foreach (var node in nodesToSetNotWalkable)
+        {
+            Stage.Grid.changeNodeType(node.x, node.y, Array2DEditor.nodeType.X);
+        }
+
+        Stage.StageBuilder.clearGrid();
+        TurnManager.instance.unsubscribe(this);
+        this.enabled = false;
     }
 
     public override void takeDamage(BasicActor from, int damage, itemID weapon = itemID.NONE) {
         damageFrom = from;
         base.takeDamage(from, damage, weapon);
     }
-
 }

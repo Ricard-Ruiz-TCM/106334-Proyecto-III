273c4d0f5203bc822ff742dd0b17e71409bd03ae
Antoni Jordi Noguera Recasens <anoguera@edu.tecnocampus.cat>
2023-05-30 03:23:41 +0200

    HASTA LOS PUTOS COJONES YA LO HE CONSEGUIDO

--- Assets/Scripts/Actor/PilarStatic.cs
+++ Assets/Scripts/Actor/PilarStatic.cs
@@ -5,16 +5,14 @@ public class PilarStatic : StaticActor {
 
     [SerializeField] private int pilarHeight = 0;
     [SerializeField] private GameObject pilar;
-
+     
     private BasicActor damageFrom;
     private Node pilarNode;
-    private Animator pilarAnimator;
 
     protected override void Start()
     {        
         base.Start();
         pilarNode = Stage.StageBuilder.getGridNode(this.transform.position);
-        pilarAnimator = this.GetComponent<Animator>();
     }
 
     public override void onActorDeath() {
@@ -22,17 +20,8 @@ public class PilarStatic : StaticActor {
         Vector2 direction = Stage.StageBuilder.getDirection(this.transform.position, damageFrom.transform.position);
         direction = direction * pilarHeight;
         Node endPoint = Stage.StageBuilder.getGridNode(pilarNode.x + (int)direction.x, pilarNode.y + (int)direction.y);
-        Debug.Log("ROTATION OF PILAR: " + transform.rotation.y);
-        pilarAnimator.SetTrigger("GoDown");
 
-        pilar.transform.rotation.SetLookRotation(Stage.StageBuilder.getGridPlane(endPoint).transform.position);
-        //transform.LookAt(new Vector3(endPoint.x, endPoint.y, transform.position.z));
-
-        //transform.rotation.SetLookRotation(Vector3.zero);
-
-        Debug.Log("ROTATION OF PILAR: " + transform.rotation.y);
-
-        
+        theCollumnGoesDown();
 
         List<Node> nodesToSetNotWalkable = Stage.Pathfinder.FindPath(pilarNode, endPoint, false);
 
@@ -48,18 +37,14 @@ public class PilarStatic : StaticActor {
         this.enabled = false;
     }
 
-    /*private void lookAtObjective(Node endPoint) 
-    {
-        Quaternion targetRotation;
-
-        Vector3 targetPoint = new Vector3(endPoint.x, endPoint.y, transform.position.z) - transform.position;
-
-        targetRotation = Quaternion.LookRotation(-targetPoint, Vector3.up);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 2.0f);
-    }*/
-
     public override void takeDamage(BasicActor from, int damage, itemID weapon = itemID.NONE) {
         damageFrom = from;
         base.takeDamage(from, damage, weapon);
+    }
+
+    private void theCollumnGoesDown()
+    {
+        pilar.transform.LookAt(damageFrom.transform);
+        pilar.transform.Rotate(0, 90, -90);
     }
 }

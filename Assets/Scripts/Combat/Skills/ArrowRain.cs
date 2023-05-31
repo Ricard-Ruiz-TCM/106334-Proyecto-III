d01e3ff178c64553ed88482fedb4f58d3040bfd2
RocFauconTuron <rfauconi@edu.tecnocampus.cat>
2023-05-31 17:09:37 +0200

    arrow

--- Assets/Scripts/Combat/Skills/ArrowRain.cs
+++ Assets/Scripts/Combat/Skills/ArrowRain.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "ArrowRain", menuName = "Combat/Skills/ArrowRain")]
-public class ArrowRain : Skill {
-
+public class ArrowRain : Skill 
+{
     public override void action(BasicActor from, Node to) {
-
+        uCore.Particles.PlayParticles("ParticulasFlechas", new Vector3(Stage.StageBuilder.getGridPlane(to).position.x, Stage.StageBuilder.getGridPlane(to).position.y + 1.6f, Stage.StageBuilder.getGridPlane(to).position.z));
         List<Node> neight = Stage.Grid.getNeighbours(to);
         // Appy damage to neightbours 
         foreach (Node node in neight) {

9508fa79bc9b0e85d4c60f71f73323f139756fa1
RocFauconTuron <rfauconi@edu.tecnocampus.cat>
2023-06-01 21:04:12 +0200

    talonDeAquiles

--- Assets/Scripts/Combat/Buffs/MidDamage.cs
+++ Assets/Scripts/Combat/Buffs/MidDamage.cs
@@ -1,13 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.VFX;
 
 [CreateAssetMenu(fileName = "MidDamage", menuName = "Combat/Buffs/Mid Damage")]
-public class MidDamage : ModBuff {
+public class MidDamage : ModBuff 
+{
+    [SerializeField] Material mat;
+    Material previousMat;
 
-    public override void onApply(BasicActor me) {
+    public override void onApply(BasicActor me) 
+    {
+        previousMat = me.GetComponent<WeaponHolder>()._gladius.GetComponent<MeshRenderer>().material;
+        me.GetComponent<WeaponHolder>()._gladius.GetComponent<MeshRenderer>().material = mat;
+        me.GetComponent<WeaponHolder>()._gladius.transform.GetChild(0).gameObject.SetActive(true);
         Debug.Log("TODO: Apply Invisible Feedback + extras.");
     }
 
-    public override void onRemove(BasicActor me) {
+    public override void onRemove(BasicActor me) 
+    {
+        me.GetComponent<WeaponHolder>()._gladius.GetComponent<MeshRenderer>().material = previousMat;
+        me.GetComponent<WeaponHolder>()._gladius.transform.GetChild(0).gameObject.SetActive(false);
         Debug.Log("TODO: Remove Invisible Feedback");
     }
 

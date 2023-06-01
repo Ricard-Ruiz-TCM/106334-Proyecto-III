5bf5d886465472af9cb44c857185514f0b641bb4
Ricard-Ruiz-TCM <83354017+Ricard-Ruiz-TCM@users.noreply.github.com>
2023-06-01 20:33:14 +0200

    loop para la anim del enemigo y otras cosas

--- Assets/Scripts/Scenes/SceneMenuManager.cs
+++ Assets/Scripts/Scenes/SceneMenuManager.cs
@@ -2,28 +2,10 @@ using UnityEngine;
 
 public class SceneMenuManager : MonoBehaviour {
 
-    [SerializeField, Header("Tutorial Stage:")]
-    private StageData _tutorial;
-
-    [SerializeField, Header("Continue Button:")]
-    private GameObject _continue;
-
-    // Unity Awake
-    void Awake() {
-        if (!uCore.GameManager.ExistGameData()) {
-            _continue.SetActive(false);
-        }
-    }
 
     /** Buttons */
-    public void BTN_Continue() {
-        uCore.GameManager.LoadGameData();
-        uCore.Director.LoadSceneAsync(gameScenes.StageSelector);
-    }
-
     public void BTN_NewGame() {
-        uCore.GameManager.StageSelected(_tutorial);
-        uCore.Director.LoadSceneAsync(gameScenes.Stage);
+        uCore.Director.LoadSceneAsync(gameScenes.Stage0);
     }
 
     public void BTN_Settings() {

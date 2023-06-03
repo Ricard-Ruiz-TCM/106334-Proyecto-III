109fb11f3d730f5e9b5d91f4b5c0670bc08e7854
Antoni Jordi Noguera Recasens <anoguera@edu.tecnocampus.cat>
2023-06-03 03:37:45 +0200

    Delimitations&Spawns(Primitive)

---
+++ Assets/Scripts/DelimitationScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelimitationScript : MonoBehaviour
+{
+    [SerializeField] List<GameObject> delimitations;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Stage.StageBuilder._grid != null)
+        {
+            foreach(var delimitation in delimitations)
+            {
+                Node myNode = Stage.StageBuilder.getGridNode(delimitation.transform.position);
+                Stage.Grid.changeNodeType(myNode.x, myNode.y, Array2DEditor.nodeType.X);
+                Stage.StageBuilder.clearGrid();
+                Destroy(delimitation);
+            }            
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+    [SerializeField] List<GameObject> spawns;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Stage.StageBuilder._grid != null)
+        {
+            foreach(var spawn in spawns)
+            {
+                Node myNode = Stage.StageBuilder.getGridNode(spawn.transform.position);
+                Stage.Grid.changeNodeType(myNode.x, myNode.y, Array2DEditor.nodeType.P);
+                Stage.StageBuilder.clearGrid();
+                Destroy(spawn);
+            }
+        }
+    }
+}

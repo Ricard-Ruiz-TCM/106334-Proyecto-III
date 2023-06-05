c93804b8194d89e8d4a8093a1f1225e1508b3bf2
Ricard-Ruiz <rruizc@edu.tecnocampus.cat>
2023-06-05 13:16:53 +0200

    Create SingleGridModifier.cs

---
+++ Assets/Scripts/Grid/SingleGridModifier.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Array2DEditor;
+
+public class SingleGridModifier  : MonoBehaviour{
+
+    [SerializeField]
+    private nodeType _newType;
+
+    // Unity OnEnable
+    void OnEnable() {
+        GridBuilder.onGridBuild += changeNode;
+    }
+
+    // Unity OnDisable
+    void OnDisable() {
+        GridBuilder.onGridBuild -= changeNode;
+    }
+
+    /** Método para cambair el nodo de tipo al asignado */
+    private void changeNode() {
+        Stage.Grid.changeNodeType(transform.position, _newType);
+    }
+
+}

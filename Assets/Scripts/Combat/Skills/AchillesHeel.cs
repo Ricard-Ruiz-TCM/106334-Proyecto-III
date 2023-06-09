9162638392e1c0b751cc5123527b0712dc6b42db
Ricard-Ruiz <rruizc@edu.tecnocampus.cat>
2023-06-09 10:25:22 +0200

    AchillesHeel Updated to make it to attack others

--- Assets/Scripts/Combat/Skills/AchillesHeel.cs
+++ Assets/Scripts/Combat/Skills/AchillesHeel.cs
@@ -5,6 +5,10 @@ public class AchillesHeel : Skill {
 
     public override void action(BasicActor from, Node to) {
         ((Actor)from).buffs.applyBuffs((Actor)from, buffsID.MidDamage);
+        BasicActor target = Stage.StageManager.getActor(to);
+        if (target != null) {
+            target.takeDamage((Actor)from, from.totalDamage());
+        }
         from.endAction();
     }
 

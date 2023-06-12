e00af79a9fef0907d21742ae5ffbbe7916c11a87
Ricard-Ruiz <rruizc@edu.tecnocampus.cat>
2023-06-12 11:06:08 +0200

    disarm sin sangre

--- Assets/Scripts/Combat/Skills/Disarm.cs
+++ Assets/Scripts/Combat/Skills/Disarm.cs
@@ -14,10 +14,6 @@ public class Disarm : Skill {
             // the second argument, upwards, defaults to Vector3.up
             Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
 
-            if (!target.GetComponent<StaticActor>()) {
-                GameObject blood = Instantiate(bloodPrefab, new Vector3(target.transform.position.x, target.transform.position.y + 0.8f, target.transform.position.z), rotation);
-                Destroy(blood, 2f);
-            }
             var lookPos = target.transform.position - from.transform.position;
             lookPos.y = 0;
             from.transform.rotation = Quaternion.LookRotation(lookPos);

7768ce373a5d00ebcd17c121360302b3e8c6b8c2
RocFauconTuron <rfauconi@edu.tecnocampus.cat>
2023-06-14 18:26:37 +0200

    tortuga vfx

--- Assets/Scripts/Combat/Buffs/ArrowProof.cs
+++ Assets/Scripts/Combat/Buffs/ArrowProof.cs
@@ -1,14 +1,23 @@
 ﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "ArrowProof", menuName = "Combat/Buffs/ArrowProof")]
-public class ArrowProof : Buff {
+public class ArrowProof : Buff 
+{
+    [SerializeField] GameObject shieldPrefab;
+    [SerializeField] GameObject shieldVFX;
 
-    public override void onApply(BasicActor me) {
+    public override void onApply(BasicActor me) 
+    {
         Debug.Log("TODO: Apply ArrowProof Feedback");
+        shieldVFX = Instantiate(shieldPrefab, Vector3.zero, Quaternion.Euler(0, 90f, 0));
+        shieldVFX.transform.parent = me.transform;
+        shieldVFX.transform.localPosition = new Vector3(0, 0, 0.734f);
     }
 
     public override void onRemove(BasicActor me) {
         Debug.Log("TODO: Remove ArrowProof Feedback");
+        shieldVFX.GetComponent<Animator>().SetTrigger("noBuff");
+        Destroy(shieldVFX, 0.5f);
     }
 
     public override void startTurnEffect(BasicActor me) {

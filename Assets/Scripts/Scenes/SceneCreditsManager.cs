d5d1d0b1aaa8e8e9d98f0d7f3051efd91b87a869
Ricard-Ruiz <rruizc@edu.tecnocampus.cat>
2023-06-15 20:54:54 +0200

    escenas bonitas :3

--- Assets/Scripts/Scenes/SceneCreditsManager.cs
+++ Assets/Scripts/Scenes/SceneCreditsManager.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class SceneCreditsManager : MonoBehaviour {
 
+    private void Start() {
+        FadeFX.instance.FadeOut();
+    }
+
     /** Buttons */
     public void BTN_Back() {
         uCore.Director.LoadSceneAsync(gameScenes.Menu);
